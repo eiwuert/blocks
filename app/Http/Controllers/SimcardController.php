ba7acd89f4148d4e5fe1cc72f5b1d2872924fb73
lespoir <stiven140@hotmail.com>
2016-08-17 12:58:40 +0000

    adding worker

--- app/Http/Controllers/SimcardController.php
+++ app/Http/Controllers/SimcardController.php
@@ -275,7 +275,7 @@ class SimcardController extends Controller
             $url = parse_url(getenv('CLOUDAMQP_URL'));
             $conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
             $ch = $conn->channel();
-            
+            $exchange = 'amq.direct';
             $msg_body = 'the body';
             $msg = new AMQPMessage($msg_body, array('content_type' => 'text/plain', 'delivery_mode' => 2));
             $ch->basic_publish($msg, $exchange);
