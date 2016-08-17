b3fb09656e9561fdb55e989627424a4f6641853f
lespoir <stiven140@hotmail.com>
2016-08-17 15:39:04 +0000

    adding worker

--- app/Worker.php
+++ app/Worker.php
@@ -27,6 +27,9 @@ class Worker
             false,              #exclusive - queues may only be accessed by the current connection
             false               #auto delete - the queue is deleted when all consumers have finished using it
             );
+            var_dump(scandir("public/files",1));
+            var_dump("----------------");
+            var_dump(scandir("public/files/simcards",1));
         while(true) {
             $retrived_msg = $channel->basic_get($queue_name)->body;
             if($retrived_msg != null){
