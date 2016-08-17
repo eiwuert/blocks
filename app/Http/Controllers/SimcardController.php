669cdc54114b9ecc35271e2a34474ecbe62b56b9
lespoir <stiven140@hotmail.com>
2016-08-17 12:35:52 +0000

    adding worker

--- app/Http/Controllers/SimcardController.php
+++ app/Http/Controllers/SimcardController.php
@@ -18,6 +18,8 @@ use DateTime;
 use Auth;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
 class SimcardController extends Controller
 {
@@ -269,49 +271,21 @@ class SimcardController extends Controller
     public function subir_archivo(Request $request){
         if ($request->hasFile('archivo_simcard'))
         {
-            $request->file('archivo_simcard')->move("files/simcards"); 
-            /*
-            $files = scandir("files/simcards",1);
-            $files = array_diff($files, array('.', '..'));
-            $files->each(function($file) {
-                $rows = Excel::selectSheetsByIndex(0)->load("files/simcards/" . $file, function($reader) {})->get();
-                global $request,$counter_filas,$filas_buenas,$filas_malas,$errores,$msg;
-                $counter_filas = 0; $filas_buenas = 0; $filas_malas=0; $msg = ""; $errores = "";
-                $rows->each(function($row) {
-                    global $request,$counter_filas,$filas_buenas,$filas_malas,$errores,$msg;
-                    try{
-                        $counter_filas++;
-                        $fecha_adjudicacion = $row->fecha_adjudicacion;
-                        if($fecha_adjudicacion != null){
-                            $fecha_adjudicacion = $fecha_adjudicacion->format('Y-m-d');
-                        }
-                        $fecha_activacion = $row->fecha_activacion;
-                        if($fecha_activacion != null){
-                            $fecha_activacion = $fecha_activacion->format('Y-m-d');
-                        }
-                        $fecha_asignacion = $row->fecha_asignacion;
-                        if($fecha_asignacion != null){
-                            $fecha_asignacion = $fecha_asignacion->format('Y-m-d');
-                        }
-                        $simcard = new Simcard(array("ICC" => $row->icc, "numero_linea" => $row->numero_linea,"categoria" => $request['categoria'] ,"fecha_adjudicacion" => $fecha_adjudicacion,"fecha_activacion" => $fecha_activacion,"fecha_asignacion" => $fecha_asignacion,"paquete_ID" => $row->paquete_id,"Cliente_identificacion" => $row->cliente_identificacion));
-                        $simcard->save();
-                        $filas_buenas++;
-                    }catch(\Exception $e){
-                        if($e->getCode() == 23000){
-                            $errores = $errores . $counter_filas . ":  ICC ya registrada\n"; 
-                        }else{
-                            $errores = $errores . $counter_filas . ": " . $e->getMessage() ."\n";    
-                        }
-                        $filas_malas++;
-                    }
-                });
-                unlink("files/simcards/" . $file);
-            });
-            $msg = $msg . "Cantidad de registros añadidos: " . $filas_buenas . "\n";
-            $msg = $msg . "Cantidad de registros con errores: " . $filas_malas . "\n";
-            $msg = $msg . "ERRORES: \n";
-            $msg = $msg . $errores;
-            */
+            //$request->file('archivo_simcard')->move("files/simcards"); 
+            $url = parse_url(getenv('CLOUDAMQP_URL'));
+            $conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
+            $ch = $conn->channel();
+            
+            $exchange = 'amq.direct';
+            $queue = 'basic_get_queue';
+            $ch->queue_declare($queue, false, true, false, false);
+            $ch->exchange_declare($exchange, 'direct', true, true, false);
+            $ch->queue_bind($queue, $exchange);
+            
+            $msg_body = 'the body';
+            $msg = new AMQPMessage($msg_body, array('content_type' => 'text/plain', 'delivery_mode' => 2));
+            $ch->basic_publish($msg, $exchange);
+            
             return \Redirect::route('simcard')->with('subiendo_archivo' ,true);
         }
     }
