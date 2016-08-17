320ca6a241e68458e50c08c0822bdebc07abf030
lespoir <stiven140@hotmail.com>
2016-08-17 20:28:52 +0000

    TODO notifications

--- app/Jobs/SimcardFileUpload.php
+++ app/Jobs/SimcardFileUpload.php
@@ -27,8 +27,8 @@ class SimcardFileUpload extends Job implements SelfHandling
      */
     public function handle()
     {
-        var_dump($this->path);
-        /*$rows = Excel::selectSheetsByIndex(0)->load($this->path, function($reader) {})->get();
+        
+        $rows = Excel::selectSheetsByIndex(0)->load($this->path, function($reader) {})->get();
         global $request,$counter_filas,$filas_buenas,$filas_malas,$errores,$msg;
         $counter_filas = 0; $filas_buenas = 0; $filas_malas=0; $msg = ""; $errores = "";
         $rows->each(function($row) {
@@ -61,7 +61,7 @@ class SimcardFileUpload extends Job implements SelfHandling
         });
         var_dump("Filas buenas: " . $filas_buenas);
         var_dump("Errores: " . $errores);
-        unlink($this->path);*/
+        unlink($this->path);
     }
     
     public function failed()
